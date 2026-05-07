3be2046b031fcebb528c36ad2b57f4cb5f6ea10f
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-05-07 21:04:29 +0100

    Implement TryFrom<&[u8]> for Header

--- src/io.rs
+++ src/io.rs
@@ -189,6 +189,14 @@ impl Deserialize for Header {
     }
 }
 
+impl TryFrom<&[u8]> for Header {
+    type Error = Error;
+
+    fn try_from(src: &[u8]) -> Result<Self, Self::Error> {
+        Self::deserialize(src)
+    }
+}
+
 impl Deserialize for NonZeroU64 {
     type Error = Error;
 
