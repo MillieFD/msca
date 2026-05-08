cdb74c9c7e1c800f04c8aff910fb0eb808f6b907
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-05-08 12:20:53 +0100

    Refactor deserialize method to improve error handling for truncated data

--- src/io.rs
+++ src/io.rs
@@ -456,12 +456,12 @@ impl Deserialize for NonZeroU64 {
     fn deserialize(src: &[u8]) -> Result<Self, Self::Error> {
         let buf = src
             .get(0..size_of::<Self>())
-            .ok_or_else(Error::Truncated {
+            .ok_or(Error::Truncated {
                 expected: size_of::<Self>(),
                 actual: src.len(),
             })?
             .try_into()?;
-        u64::from_le_bytes(buf).try_into()
+        u64::from_le_bytes(buf).try_into().map_err(Error::Convert)
     }
 }
 
