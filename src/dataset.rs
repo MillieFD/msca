96a5909d7b42683cab3bf6c8e770344dd7074208
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-05-08 08:36:53 +0100

    Add Dataset stub with manifest and io::file fields

--- src/dataset.rs
+++ src/dataset.rs
@@ -7,3 +7,25 @@ BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the conditions of the LICENSE are met.
 */
+
+//! todo mod doc
+
+use crate::io::File;
+use crate::manifest::Manifest;
+use minicbor::{CborLen, Decode, Encode};
+
+/* ------------------------------------------------------------------------------ Public Exports */
+
+/// todo struct doc comment
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Encode, Decode, CborLen)]
+pub struct Dataset {
+    /// todo field doc comment
+    #[n(0)]
+    manifest: Manifest,
+    /// todo field doc comment
+    #[n(1)]
+    file: File,
+}
+
+impl Dataset {}
