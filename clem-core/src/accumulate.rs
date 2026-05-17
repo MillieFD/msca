a05cfcaafbfb7dde68fba5a12b32ef5b0faa8ccc
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-05-17 08:46:53 +0100

    Refactor OptBitVec, Seq, and OptSeq to use where clauses to express trait bounds on generic types

--- clem-core/src/accumulate.rs
+++ clem-core/src/accumulate.rs
@@ -83,8 +83,11 @@ impl<T> Default for OptInSitu<T> {
 /// buffer for supported niche types; no validity mask required. Implementors are advised to use
 /// niche-optimised types when possible to improve storage efficiency and random read performance.
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub(crate) struct OptBitVec<T: Unfold + Default> {
 #[derive(Debug, Default, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub(crate) struct OptBitVec<T>
+where
+    T: Unfold + Default,
+{
     /// Validity mask where `true → `[`Some`] and `false → `[`None`].
     #[cfg_attr(
         feature = "serde",
@@ -144,8 +147,11 @@ pub(crate) struct OptBitVec<T: Unfold + Default> {
 ///
 /// [1]: https://doc.rust-lang.org/reference/dynamically-sized-types.html
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub(crate) struct Seq<T: Unfold> {
 #[derive(Debug, Default, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub(crate) struct Seq<T>
+where
+    T: Unfold,
+{
     /// Cumulative end offsets. `offsets[i]` marks the inclusive end of element `i` and the
     /// exclusive start of element `i + 1`.
     #[cfg_attr(
@@ -180,8 +186,11 @@ pub(crate) struct Seq<T: Unfold> {
 ///
 /// [1]: https://doc.rust-lang.org/reference/dynamically-sized-types.html
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-pub(crate) struct OptSeq<T: Unfold> {
 #[derive(Debug, Default, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub(crate) struct OptSeq<T>
+where
+    T: Unfold,
+{
     /// Cumulative end offsets per row; [`None`] marks a null row (no data appended).
     #[cfg_attr(
         feature = "serde",
