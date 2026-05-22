176b5aa6cde2d0fc567d04a8ab80fdaeefa7d95d
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-05-23 00:35:25 +0100

    Implement insert and checked_insert methods for Dictionary, along with utility methods count, is_empty, and discard

--- clem-core/src/dictionary.rs
+++ clem-core/src/dictionary.rs
@@ -34,3 +34,53 @@ where
     /// [`Sector`] of the bound dictionary schema segment.
     pub(crate) schema: Sector,
 }
+
+impl<K, I> Dictionary<K, I>
+where
+    K: Unfold + Ord,
+    I: Serialize,
+{
+    /// Insert a new entry pair into the [`Dictionary`].
+    ///
+    /// - Returns [`None`] if the map did not contain this key.
+    /// - Returns [`Some`] wrapping the previous value if the key was present.
+    ///
+    /// Refer to [`BTreeMap::insert`] documentation for more details. Use [`checked_insert`][1] for
+    /// cases where overwriting existing values is undesirable and should return an error.
+    ///
+    /// [1]: Self::checked_insert
+    pub fn insert(&mut self, key: K, item: I) -> Option<I> {
+        self.entries.insert(key, item)
+    }
+
+    /// Insert a new entry into the [`Dictionary`].
+    ///
+    /// Returns an immutable reference to the inserted [`item`](I) on success, or [`Error`] if the
+    /// [`map`](BTreeMap) already contains the specified [`key`](K). Existing items are **not**
+    /// overwritten.
+    ///
+    /// Use [`insert`](Self::insert) for cases where updating existing values in situ is desired.
+    pub fn checked_insert(&mut self, key: K, value: I) -> Result<&I, Error> {
+        match self.entries.entry(key) {
+            Entry::Vacant(entry) => Ok(&*entry.insert(value)),
+            Entry::Occupied(entry) => Error::Collision.into(),
+        }
+    }
+
+    /// Returns the number of accumulated entries.
+    pub fn count(&self) -> u64 {
+        self.entries.len() as u64
+    }
+
+    /// Returns `true` if the [`Dictionary`] contains no entries.
+    pub fn is_empty(&self) -> bool {
+        self.entries.is_empty()
+    }
+
+    /// Reinitialise the [`Dictionary`] without writing to disk. All data is permanently lost.
+    ///
+    /// Note that this method may not affect the allocated capacity of the underlying storage.
+    pub fn discard(&mut self) {
+        self.entries.clear();
+    }
+}
