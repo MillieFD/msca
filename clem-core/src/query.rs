4977d9f68a140f7eeec590fe22a3a6ccd79802cf
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-06-08 17:14:32 +0100

    Enhance query.rs mod doc to clarify filter evaluation and IO behavior

--- clem-core/src/query.rs
+++ clem-core/src/query.rs
@@ -8,13 +8,20 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the conditions of the LICENSE are met.
 */
 
-//! The [`Query`] API provides a composable interface for reading data from any [clem](crate) file.
-//! Conditions are chained method-by-method and evaluated lazily; no file IO occurs until
-//! `.read().await` is called.
+//! A composable [`Query`] interface to [read](Read) data from any [clem](crate) file.
+//!
+//! ---
+//!
+//! Each new [`Query`] begins with **every** column and **every** buffer from the specified schema.
+//! [`Filter`] functions are then applied subtractively to reduce the result set. Some filters are
+//! evaluated **before** file IO to remove individual buffers or entire columns using [manifest]
+//! statistics. Other filters are attached to the relevant column and evaluated lazily **during**
+//! buffer [deserialization](Deserialize). No file IO is executed until [`read`](Query::read) is
+//! awaited.
 //!
 //! ```rust,ignore
 //! let results = dataset
-//!     .query("schema_name")
+//!     .query("schema_name")?
 //!     .select(["latitude", "longitude", "temperature"])
 //!     .range("temperature", 10.0..=20.0)
 //!     .eq("active", true)
