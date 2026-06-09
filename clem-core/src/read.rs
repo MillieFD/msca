f24db07921beb5ba62f437bcc6e5ad9d1c7164f2
MillieFD <155637089+MillieFD@users.noreply.github.com>
2026-06-09 18:42:52 +0100

    Add Outcome enum for deserialization results in read module

--- clem-core/src/read.rs
+++ clem-core/src/read.rs
@@ -7,3 +7,20 @@ BSD 3-Clause License, Copyright (c) 2026, Amelia Fraser-Dale
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the conditions of the LICENSE are met.
 */
+
+use crate::io::{self, Deserialize};
+use crate::manifest::Buffer;
+use crate::query::Filter;
+/// The result of [deserializing](Deserialize) a single [`Item`](I) from the [`Read`] stream.
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum Outcome<I> {
+    /// A [deserialized](Deserialize::deserialize) [`Item`](I) which satisfies every [`Filter`].
+    Success(I),
+    /// The [`Item`](I) was rejected by one or more [filters](Filter) during [deserialization][1].
+    ///
+    /// [1]: Deserialize::deserialize
+    Excluded,
+    /// Every candidate [`Column`](query::Column) [`Item`](I) has been [`Read`].
+    Finished,
+}
